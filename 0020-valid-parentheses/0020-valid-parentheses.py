cc0a264892d1130d12b09abbb840a9e99ac64c3c
9keyyyy <62213813+9keyyyy@users.noreply.github.com>
2025-05-10 16:38:32 +0900

    Time: 3 ms (51.05%), Space: 12.6 MB (56.91%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        q = deque()
+
+        pair = [["(", ")"], ["{", "}"], ["[", "]"]]
+        
+        for c in s:
+            for opened, closed in pair:
+                if c == opened:
+                    q.append(opened)
+                elif c == closed:
+                    if not q:
+                        return False
+                    cur = q.pop()
+                    if cur != opened:
+                        return False
+        if q:
+            return False
+        else:
+            return True
