64672776fc547896888cf6b8186595930314681c
9keyyyy <62213813+9keyyyy@users.noreply.github.com>
2025-05-15 23:44:56 +0900

    Time: 0 ms (100%), Space: 13 MB (58.15%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -6,37 +6,16 @@ class Solution(object):
         :rtype: int
         """
         n = len(nums)
-        
         l, r = 0, n-1
-        while (r-l) > 1:
+
+        while l <= r:
             mid = (l+r)//2
-            print(mid, nums[mid])
             if nums[mid] == target:
                 return mid
 
             if nums[mid] < target:
-                l = mid
+                l = mid + 1
             else:
-                r = mid
+                r = mid - 1
 
-
-        if nums[r] < target:
-            return r+1
-        if nums[l] > target:
-            return 0
-        if nums[r] == target:
-            return r
-        if nums[l] == target:
-            return l
-
-        return l+1
-        # mid = n // 2
-
-        # while mid <= n or mid > 0:
-        #     if nums[mid] < target:
-        #         mid = (mid + n) // 2
-        #     else:
-        #         mid = mid // 2
-
-
-        
+        return l
