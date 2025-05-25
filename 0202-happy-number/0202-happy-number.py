8e62f5313308c994626f8180c2fed5ddbbe7f613
9keyyyy <62213813+9keyyyy@users.noreply.github.com>
2025-05-25 23:40:27 +0900

    Time: 2 ms (68.21%), Space: 12.4 MB (79.55%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        arr = defaultdict(int)
+        arr[n] = 1
+
+
+        while True:
+            total = 0
+            while n != 0:
+                cur = n % 10
+                total += cur * cur
+                n = n // 10
+
+            n = total
+
+
+            if n == 1:
+                return True
+            if n in arr:
+
+                return False
+            arr[n] = 1
+
+        
