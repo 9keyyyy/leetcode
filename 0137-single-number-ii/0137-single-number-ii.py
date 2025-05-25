5d9a177baf013694ce754bc269a7013792031d32
9keyyyy <62213813+9keyyyy@users.noreply.github.com>
2025-05-25 23:13:55 +0900

    Time: 3 ms (73.78%), Space: 14.7 MB (5.53%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        arr = defaultdict(int)
+
+        for n in nums:
+            arr[n] += 1
+
+        for k, v in arr.items():
+            if v == 1:
+                return k
