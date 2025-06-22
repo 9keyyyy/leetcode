067e35521d555c17b3cb32880ea4bb5991b9ec67
9keyyyy <62213813+9keyyyy@users.noreply.github.com>
2025-06-22 15:34:22 +0900

    Time: 0 ms (100%), Space: 12.4 MB (82.54%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -5,16 +5,17 @@ class Solution(object):
         :rtype: int
         """
         dp = [0]*len(nums)
+        n = len(nums)
 
-        if len(nums) == 1:
+        if n == 1:
             return nums[0]
 
 
         dp[0] = nums[0]
         dp[1] = max(nums[0], nums[1])
 
-        for i in range(2, len(nums)):
+        for i in range(2, n):
             dp[i] = max(dp[i-2] + nums[i], dp[i-1])
-        print(dp)
-        return dp[len(nums)-1]
+
+        return dp[-1]
         
