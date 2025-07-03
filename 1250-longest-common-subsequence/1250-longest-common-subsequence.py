e2b7b898b97a873b3d8b8fb567240c5099a4f77e
9keyyyy <62213813+9keyyyy@users.noreply.github.com>
2025-07-03 20:54:22 +0900

    Time: 550 ms (49.35%), Space: 33.7 MB (66.45%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type text2: str
         :rtype: int
         """
+        """
         # 메모이제이션을 위한 딕셔너리
         memo = {}
         
@@ -29,6 +30,24 @@ class Solution(object):
             return result
         
         return lcs(0, 0)
+        """
+
+        m, n = len(text1), len(text2)
+        
+        # DP 테이블 초기화
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        # 앞에서부터 채우기
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i-1] == text2[j-1]:
+                    # 현재 문자가 같으면: 이전 대각선 + 1
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    # 현재 문자가 다르면: 위쪽 또는 왼쪽 중 최대값
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        
+        return dp[m][n]
 
 
                     
