e9a5d1a28f8db93ba792c9e41cabf07f9262bbc2
9keyyyy <62213813+9keyyyy@users.noreply.github.com>
2025-07-28 23:14:43 +0900

    Time: 285 ms (22.39%), Space: 52.9 MB (86.57%) - LeetHub

---
+++ 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def maxSubArrayLen(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        prefix_sum = longest_subarray = 0
+        indices = {}
+        
+        for i, num in enumerate(nums):
+            prefix_sum += num
+            
+            if prefix_sum == k:
+                longest_subarray = i + 1
+                
+            if prefix_sum - k in indices:
+                longest_subarray = max(longest_subarray, i - indices[prefix_sum - k])
+
+            if prefix_sum not in indices:
+                indices[prefix_sum] = i
+        
+        return longest_subarray
+
+        
