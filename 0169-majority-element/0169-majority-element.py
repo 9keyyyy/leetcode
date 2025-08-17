27ce6bcaaf9ac745e4022df48ca31e960f23c19b
9keyyyy <62213813+9keyyyy@users.noreply.github.com>
2025-08-17 16:24:19 +0900

    Time: 5 ms (64.5%), Space: 19.7 MB (18.97%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,24 +1,11 @@
 class Solution:
-    def majorityElement(self, nums, lo=0, hi=None):
-        def majority_element_rec(lo, hi):
-            # base case; the only element in an array of size 1 is the majority
-            # element.
-            if lo == hi:
-                return nums[lo]
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        major_len = math.ceil(n/2)
+        hmap = defaultdict(int)
 
-            # recurse on left and right halves of this slice.
-            mid = (hi - lo) // 2 + lo
-            left = majority_element_rec(lo, mid)
-            right = majority_element_rec(mid + 1, hi)
+        for n in nums:
+            hmap[n] += 1
+            if hmap[n] >= major_len:
+                return n
 
-            # if the two halves agree on the majority element, return it.
-            if left == right:
-                return left
-
-            # otherwise, count each element and return the "winner".
-            left_count = sum(1 for i in range(lo, hi + 1) if nums[i] == left)
-            right_count = sum(1 for i in range(lo, hi + 1) if nums[i] == right)
-
-            return left if left_count > right_count else right
-
-        return majority_element_rec(0, len(nums) - 1)
