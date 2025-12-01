6f732af7f9553ed7950a32faacdec98fc9fb9e3c
9keyyyy <62213813+9keyyyy@users.noreply.github.com>
2025-12-01 15:17:43 +0800

    Time: 6998 ms (26.85%), Space: 12.4 MB (79.73%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -12,10 +12,10 @@ class Solution(object):
         visited = [[0]*m for _ in range(n)]
 
         def dfs(y, x, cur):
-            cur += board[y][x]
             if cur == word:
-                print(cur, word, visited)
                 return True
+            if cur != word[:len(cur)]:
+                return False
             
             for i in range(4):
                 nx = x + dx[i]
@@ -23,10 +23,8 @@ class Solution(object):
 
                 if nx < 0 or nx >= m or ny < 0 or ny >= n or visited[ny][nx]:
                     continue
-                if cur + board[ny][nx] != word[:len(cur)+1]:
-                    continue
                 visited[ny][nx] = 1
-                if dfs(ny, nx, cur):
+                if dfs(ny, nx, cur+board[ny][nx]):
                     return True
                 visited[ny][nx] = 0
             
@@ -36,7 +34,7 @@ class Solution(object):
             for j in range(m):
                 if board[i][j] == word[0]:
                     visited[i][j] = 1
-                    if dfs(i, j, ""):
+                    if dfs(i, j, word[0]):
                         return True
                     visited[i][j] = 0
         
