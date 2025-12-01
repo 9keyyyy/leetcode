1e1ea2531d13bd3c3d2f6c6741b36f99b67013be
9keyyyy <62213813+9keyyyy@users.noreply.github.com>
2025-12-01 15:08:14 +0800

    Time: 6531 ms (50.97%), Space: 12.3 MB (79.73%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,40 +1,44 @@
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        dx = [-1, 0, 0, 1]
-        dy = [0, 1, -1, 0]
-
-        m = len(board[0])
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
         n = len(board)
-
+        m = len(board[0])
+        dx = [-1, 0, 0, 1]
+        dy = [0, -1, 1, 0]
         visited = [[0]*m for _ in range(n)]
-        def dfs(y, x, idx):
-            visited[y][x] = 1
-            if idx == len(word)-1:
+
+        def dfs(y, x, cur):
+            cur += board[y][x]
+            if cur == word:
+                print(cur, word, visited)
                 return True
             
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
 
-                if nx < 0 or nx >= m or ny < 0 or ny >= n:
+                if nx < 0 or nx >= m or ny < 0 or ny >= n or visited[ny][nx]:
                     continue
-
-                if word[idx+1] == board[ny][nx] and not visited[ny][nx]:
-                    if dfs(ny, nx, idx+1):
-                        return True
-                
-            visited[y][x] = 0
+                if cur + board[ny][nx] != word[:len(cur)+1]:
+                    continue
+                visited[ny][nx] = 1
+                if dfs(ny, nx, cur):
+                    return True
+                visited[ny][nx] = 0
+            
             return False
-
 
         for i in range(n):
             for j in range(m):
-                if word[0] == board[i][j]:
-                    # visited[i][j] = 1
-                    if dfs(i, j, 0):
+                if board[i][j] == word[0]:
+                    visited[i][j] = 1
+                    if dfs(i, j, ""):
                         return True
-                    # visited[i][j] = 0
-
-        return False
-
-                
+                    visited[i][j] = 0
+        
+        return False   
+        
