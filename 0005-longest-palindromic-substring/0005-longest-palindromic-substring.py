270715aeef457c47782336745d14923d1bf4f7bb
9keyyyy <62213813+9keyyyy@users.noreply.github.com>
2025-12-04 15:54:15 +0800

    Time: 495 ms (36.82%), Space: 12.6 MB (56.05%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -6,28 +6,36 @@ class Solution(object):
         """
         ans = s[0]
         for i in range(len(s)):
-            start, end = i-1, i+1
-
-            if end < len(s) and s[end] == s[i]:
-                while end < len(s) and s[end] == s[i]:
-                    end += 1
-
-                if len(ans) < len(s[i:end]):
-                    ans = s[i:end]
-
-
+            cur = s[i]
+            start, end = i, i
             while True:
+                start -= 1
+                end += 1
                 if start < 0 or end >= len(s):
                     break
                 if s[start] == s[end]:
-                    if len(ans) < len(s[start:end+1]):
+                    if len(ans) < end-start+1:
                         ans = s[start:end+1]
-                
                 else:
                     break
-                
+
+        for i in range(len(s)):
+            cur = s[i]
+            start, end = i, i+1
+            if end >= len(s) or s[start] != s[end]:
+                continue
+            if len(ans) < end-start+1:
+                ans = s[start:end+1]
+            while True:
                 start -= 1
                 end += 1
-
+                if start < 0 or end >= len(s):
+                    break
+                if s[start] == s[end]:
+                    if len(ans) < end-start+1:
+                        ans = s[start:end+1]
+                        print(ans)
+                else:
+                    break
         return ans
 
