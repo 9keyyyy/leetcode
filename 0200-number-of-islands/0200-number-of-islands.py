6e8e57f3f3a8f2b0146a48aeeefa3945bdd9dd32
9keyyyy <62213813+9keyyyy@users.noreply.github.com>
2025-12-17 18:01:43 +0800

    Time: 230 ms (53.69%), Space: 29.4 MB (38.2%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,35 +4,38 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        dx = [-1, 0, 0, 1]
-        dy = [0, -1, 1, 0]
-
-        m = len(grid[0])
         n = len(grid)
+        m = len(grid[0])
         visited = [[0]*m for _ in range(n)]
 
-        def bfs(i, j):
-            q = deque()
-            q.append([i, j])
+        def bfs(b, a):
+            dx = [-1, 0, 0, 1]
+            dy = [0, -1, 1, 0]
+
+            q = deque([(b, a)])
+
             while q:
                 y, x = q.popleft()
-                
-                for i in range(4):
-                    nx = x + dx[i]
-                    ny = y + dy[i]
 
-                    if nx >= m or nx < 0 or ny < 0 or ny >= n:
+                for i in range(4):
+                    ny = y + dy[i]
+                    nx = x + dx[i]
+
+                    if ny < 0 or ny >= n or nx < 0 or nx >= m:
                         continue
-                    
-                    if not visited[ny][nx] and grid[ny][nx] == "1":
+                    if visited[ny][nx]:
+                        continue
+
+                    if grid[ny][nx] == "1":
                         visited[ny][nx] = 1
-                        q.append([ny, nx])
+                        q.append((ny, nx))
 
         ans = 0
         for i in range(n):
             for j in range(m):
-                if grid[i][j] == "1" and not visited[i][j]:
-                    bfs(i, j)
+                if not visited[i][j] and grid[i][j] == "1":
+                    visited[i][j] = 1
                     ans += 1
+                    bfs(i, j)
 
         return ans
