7aeacd1f9fb4cc4655b1cf6982918527d2e789fd
9keyyyy <62213813+9keyyyy@users.noreply.github.com>
2026-01-25 19:44:27 +0900

    Time: 0 ms (100%), Space: 12.4 MB (87.31%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,45 +1,33 @@
 class Solution(object):
-    ans = []
-    n = 0
-    letter = []
-
-    def dfs(self, comb):
-        if len(comb) == self.n:
-            self.ans.append(comb)
-            return
-
-        for j in range(len(self.letter[len(comb)])):
-            comb += self.letter[len(comb)][j]
-            self.dfs(comb)
-            comb = comb[:-1]
-
-
     def letterCombinations(self, digits):
         """
         :type digits: str
         :rtype: List[str]
         """
-        letters = {
-            2: ["a", "b", "c"],
-            3: ["d", "e", "f"],
-            4: ["g", "h", "i"],
-            5: ["j", "k", "l"],
-            6: ["m", "n", "o"],
-            7: ["p", "q", "r", "s"],
-            8: ["t", "u", "v"],
-            9: ["w", "x", "y", "z"]
+        nums = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
         }
+        n = len(digits)
 
-        self.n = len(digits)
-        self.letter = []
-        self.ans = []
-        # [["a", "b", "c"], ["d", "e", "f"]]
-        for c in digits:
-            self.letter.append(letters[int(c)])
+        def backtrack(s, ans):
+            if len(s) == n:
+                ans.append(s)
+                return
 
-        if self.n > 0:
-            self.dfs("")
+            for num in nums[digits[len(s)]]:
+                s += num
+                backtrack(s, ans)
+                s = s[:-1]
 
-        return self.ans
-            
-            
+        ans = []
+        for num in nums[digits[0]]:
+            backtrack(num, ans)
+
+        return ans
