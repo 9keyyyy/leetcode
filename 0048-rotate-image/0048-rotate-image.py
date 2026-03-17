4e827e77b48baf0ecfab19fcd8ee860220a731ad
9keyyyy <62213813+9keyyyy@users.noreply.github.com>
2026-03-17 18:56:34 +0900

    Time: 0 ms (100%), Space: 12.4 MB (81.8%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -9,13 +9,7 @@ class Solution(object):
         n -= 1
         h = 0
         for c in range(n, 0, -2):
-            # print("!!!",c)
             for i in range(c):
-                # print('cur', i)
-                # print(h, h+i)
-                # print(n-h-i, h)
-                # print(n-h, n-h-i)
-                # print(h+i, n-h)
                 tmp = matrix[h][h+i]
                 matrix[h][h+i] = matrix[n-h-i][h]
                 matrix[n-h-i][h] = matrix[n-h][n-h-i]
